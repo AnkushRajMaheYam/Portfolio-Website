42db184f3c8040f35652c7870f905bfe365d19aa
Ankush Raj <155348794+AnkushRajMaheYam@users.noreply.github.com>
2025-09-14 13:50:03 +0530

    Update mailingvariables.php

--- mailing/mailingvariables.php
+++ mailing/mailingvariables.php
@@ -1,7 +1,7 @@
 <?php
     $mail_host = "smtp.gmail.com";
     $mail_port = "465";
-    $mail_sender_email = "ankushrajmaheyam@gmail.com"; 
-    $mail_sender_password = "nvgz ixwa qpdf mfxc "; 
+    $mail_sender_email = "insert you email"; 
+    $mail_sender_password = " insert your password"; 
     $mail_sender_name = "Website Form";
 ?>
